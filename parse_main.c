a12ab81fba12911fe051a6a6c8564d83f3b549f3
Mateus Henrique De Lucas <made-luc@c2r2p2.42sp.org.br>
2026-07-03 12:39:33 -0300

    add: adding parse_args and main functions

--- parse_main.c
+++ parse_main.c
@@ -6,17 +6,9 @@
 /*   By: made-luc <made-luc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 14:06:41 by made-luc          #+#    #+#             */
-/*   Updated: 2026/07/03 10:51:01 by made-luc         ###   ########.fr       */
+/*   Updated: 2026/07/03 12:38:07 by made-luc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
-/*TODO: TRABALHAR NA MAIN:
-iniciar stack a
-iniciar parsing config
-se argc == 1: sair sem imprimir nada
-chamar parse_args
-se der erro: limpar stack + imprimir Error*/
 
 int	parse_args(int argc, char **argv, t_stack *a, t_parsing *parsing)
 {
@@ -28,10 +20,43 @@ int	parse_args(int argc, char **argv, t_stack *a, t_parsing *parsing)
 	{
 		if (!argv[i] || argv[i][0] == '\0')
 			return (1);
-		parse_flag(argv[i], parsing)
-		// se não for flag, tentar número
+		flag_result = parse_flag(argv[i], parsing);
+		if (flag_result == -1)
+			return (1);
+		if (flag_result == 0)
+		{
+			if (parse_number_token(argv[i], a))
+				return (1);
+			parsing->seen_number = true;
+		}
 		i++;
 	}
-	// verificar se viu número
+	if (parsing->seen_number == false)
+		return (1);
 	return (0);
 }
+
+int	main(int argc, char **argv)
+{
+	t_stack		a;
+	t_parsing	parsing;
+
+	if (argc < 2)
+		return (0);
+	ft_init_stack(&a);
+	init_parsing_config(&parsing);
+	if (parse_args(argc, argv, &a, &parsing))
+	{
+		ft_clear_stack(&a);
+		write(2, "Error\n", 6);
+		return (1);
+	}
+	/*
+	int		apply_strategy(t_parsing *parsing, t_stack *a);
+
+	*/
+	ft_clear_stack(&a);
+	return (0);
+}
+
+//int		apply_strategy(t_parsing *parsing, t_stack *a);
